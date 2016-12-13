26d7e4d04b079484661a20c6b04deb50c191474e
DaybreakLS <pete38@web.de>
2016-12-13 21:36:28 +0100

    Add files via upload
    -Tryndamere v1

---
+++ DayTryndamere/DAYBREAKs Tryn/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAYBREAKs_Tryn
+{
+    class Class1
+    {
+    }
+}
